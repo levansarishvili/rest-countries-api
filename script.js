cc960450028b57700eb4abff5db7dd5ad73e95ac
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-07-15 13:21:42 +0400

    feat: add custom dropdown list html and css code

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+// =============== Color Mode Switcher ===============
+const body = document.querySelector("body");
+const colorModeBtn = document.querySelector(".color-mode-btn");
+
+colorModeBtn.addEventListener("click", function () {
+  body.style.backgroundColor = "hsl(207, 26%, 17%)";
+});
+
+// =============== Custom Dropdown List ===============
+const dropDownButton = document.querySelector(".custom-dropdown__btn");
+const optionsContainer = document.querySelector(".options-wrapper");
+
+dropDownButton.addEventListener("click", function () {
+  optionsContainer.classList.toggle("hidden");
+});
