a2ab57e294e17af731e4c97884ed7b6412509703
levani_22 <levanisarishvili322@gmail.com>
2024-07-16 13:21:04 +0400

    feat: add root colors in css

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 
 // =============== Color Mode Switcher ===============
 const body = document.querySelector("body");
+const header = document.querySelector(".header");
 const colorModeBtn = document.querySelector(".color-mode-btn");
 
-colorModeBtn.addEventListener("click", function () {
-  body.style.backgroundColor = "hsl(207, 26%, 17%)";
+colorModeBtn.addEventListener("click", () => {
+  // body.classList.toggle("dark-mode");
+  // header.classList.toggle("dark-mode-header");
 });
 
 // =============== Custom Dropdown List ===============
 const dropDownButton = document.querySelector(".custom-dropdown__btn");
 const optionsContainer = document.querySelector(".options-wrapper");
 
-dropDownButton.addEventListener("click", function () {
+dropDownButton.addEventListener("click", () => {
   optionsContainer.classList.toggle("hidden");
 });
