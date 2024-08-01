9e4fe9f429a2688ec8c3e4ed3880a01afa1c2cca
levani_22 <levanisarishvili322@gmail.com>
2024-08-01 17:46:12 +0400

    feat: save color theme state in localstorage

--- js/script.js
+++ js/script.js
@@ -5,11 +5,29 @@ document.addEventListener("DOMContentLoaded", () => {
   const colorSwitcherBtn = document.querySelector(".color-switcher-btn");
 
   // =============== Color Mode Switcher ===============
+
+  let darkmode = localStorage.getItem("darkmode");
+  console.log(darkmode);
+
+  const enableDarkmode = () => {
+    body.classList.add("darkmode");
+    localStorage.setItem("darkmode", "active");
+  };
+
+  const disableDarkmode = () => {
+    body.classList.remove("darkmode");
+    localStorage.setItem("darkmode", null);
+  };
+
+  if (darkmode === "active") enableDarkmode();
+
   colorSwitcherBtn.addEventListener("click", () => {
-    body.classList.toggle("darkmode");
+    darkmode = localStorage.getItem("darkmode");
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
   });
 
   // =============== Get Country Data from API ===============
+
   // Make AJAX Call (old way)
   const request = new XMLHttpRequest();
   request.open("GET", "https://restcountries.com/v3.1/name/usa");
