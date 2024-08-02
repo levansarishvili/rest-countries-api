0c2ea0e1a98cd1fd759eaceb502df0129450b8ce
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-08-02 23:38:24 +0400

    feat: get country data from rest country api

--- js/script.js
+++ js/script.js
@@ -1,41 +1,75 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const body = document.querySelector("body");
-  const colorSwitcherBtn = document.querySelector(".color-switcher-btn");
+const body = document.querySelector("body");
+const colorSwitcherBtn = document.querySelector(".color-switcher-btn");
+const countryCardsContainer = document.querySelector(".section-country-cards");
 
-  // =============== Color Mode Switcher ===============
+// =============== Color Mode Switcher ===============
 
-  let darkmode = localStorage.getItem("darkmode");
-  console.log(darkmode);
+let darkmode = localStorage.getItem("darkmode");
 
-  const enableDarkmode = () => {
-    body.classList.add("darkmode");
-    localStorage.setItem("darkmode", "active");
-  };
+const enableDarkmode = () => {
+  body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+};
 
-  const disableDarkmode = () => {
-    body.classList.remove("darkmode");
-    localStorage.setItem("darkmode", null);
-  };
+const disableDarkmode = () => {
+  body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
+};
 
-  if (darkmode === "active") enableDarkmode();
+if (darkmode === "active") enableDarkmode();
 
-  colorSwitcherBtn.addEventListener("click", () => {
-    darkmode = localStorage.getItem("darkmode");
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
-  });
-
-  // =============== Get Country Data from API ===============
-
-  // Make AJAX Call (old way)
-  const request = new XMLHttpRequest();
-  request.open("GET", "https://restcountries.com/v3.1/name/usa");
-
-  request.send();
-
-  request.addEventListener("load", function () {
-    const [data] = JSON.parse(request.responseText);
-    console.log(data);
-  });
+colorSwitcherBtn.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
 });
+
+// =============== Get Country Data from Rest Country API ===============
+
+const renderCountry = (data) => {
+  const countryCardHtml = `
+    <a class="country-card-wrap" href="detail.html">
+           <div class="country-card">
+            <div class="country-card__img-wrap">
+               <img
+                 class="country-card__img"
+                 src="${data.flags.png}"
+                 alt="Country flag"
+               />
+             </div>
+             <div class="country-card__desc">
+               <h2 class="country-card__header">${data.name.common}</h2>
+               <div class="country-card__details">
+                 <p class="country-card__population">
+                   <strong>Population:</strong> ${(
+                     data.population / 1000000
+                   ).toFixed(1)}M
+                 </p>
+                 <p class="country-card__region">
+                   <strong>Region:</strong> ${data.region}
+                 </p>
+                 <p class="country-card__capital">
+                   <strong>Capital:</strong> ${data.capital[0]}
+                 </p>
+               </div>
+             </div>
+           </div>
+         </a>
+   `;
+
+  countryCardsContainer.insertAdjacentHTML("beforeend", countryCardHtml);
+};
+
+const getCountryData = (country) => {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => renderCountry(data[0]));
+};
+
+getCountryData("japan");
+getCountryData("usa");
+getCountryData("germany");
+getCountryData("italy");
+getCountryData("france");
+getCountryData("argentina");
